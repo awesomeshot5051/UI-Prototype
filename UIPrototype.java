818b338ab1da5877d7d99e8d97041e0cef229dc1
s412924 <traeengland01@Att.net>
2024-10-25 10:53:28 -0400

    Original file, now named UIPrototypeMainClass

--- UIPrototype.java
+++
@@ -1,6 +0,0 @@
-public class UIPrototype {
-    public static void main(String[] args) {
-    System.out.println("This is the prototype");
-    System.out.println("Hello there my friends!");
-    }
-}
