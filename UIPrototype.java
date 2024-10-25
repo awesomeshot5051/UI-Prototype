7c36dd4d1cc3fb8b1c0fddaae50c970a4e8f0f51
awesomeshot5051 <traeengland01@Att.net>
2024-10-24 21:30:57 -0400

    Initial commit

--- UIPrototype.java
+++ UIPrototype.java
@@ -1,5 +1,6 @@
 public class UIPrototype {
     public static void main(String[] args) {
     System.out.println("This is the prototype");
+    System.out.println("Hello there my friends!");
     }
 }
