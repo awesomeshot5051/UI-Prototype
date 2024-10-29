38cd007baef0c1fb086f45daa6376b1a569a6f47
awesomeshot5051 <traeengland01@Att.net>
2024-10-29 14:07:53 -0400

    Updated Phone Number Field to auto-select country based on inputted number

--- Prototype/src/main/java/FirstPage.java
+++ Prototype/src/main/java/FirstPage.java
@@ -1,14 +1,23 @@
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 import javax.swing.*;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class FirstPage implements ActionListener, Runnable {
+public class FirstPage extends Application {
+    private static final double WIDTH = 640;
+    private static final double HEIGHT = 480;
     private final JFrame page1Frame = new JFrame("Personal information");
+    private final BorderPane parent = new BorderPane();
     JTextField numberField, streetField, cityField, stateField, zipField;
     private JLabel currentAddressLabel;
     private JLabel headingLabel;
@@ -20,7 +29,6 @@ public class FirstPage implements ActionListener, Runnable {
     private JLabel middleNameLabel;
     private JTextField middleName;
     private JLabel phoneNumberLabel;
-    private JTextField phoneNumber;
     private JLabel emailLabel;
     private JTextField email;
     private JRadioButton maleRadioButton;
@@ -30,44 +38,91 @@ public class FirstPage implements ActionListener, Runnable {
     private JFormattedTextField weightFormattedTextField;
     private JButton clearButton;
     private JButton submitButton;
+    private JFormattedTextField phoneNumberField;
+
 
     FirstPage() {
         setupUI();
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Scene scene = new Scene(this.parent, WIDTH, HEIGHT);
+
+        // Set the stage title
+        stage.setTitle("JavaFX: Phone Number Input Field");
+
+        // Sets the stage scene
+        stage.setScene(scene);
+
+        // Centers stage on screen
+        stage.centerOnScreen();
+
+        // Show stage on screen
+        stage.show();
+
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        this.setupUI();
+    }
+
     private void setupUI() {
+        BorderPane borderPane = new BorderPane();
+        phoneNumberField = new JFormattedTextField();
+        phoneNumberField.setColumns(20);
+        phoneNumberField.setValue("");  // Start with empty value
+
+        // Format the number on focus loss
+        phoneNumberField.addFocusListener(new java.awt.event.FocusAdapter() {
+            @Override
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                formatPhoneNumber();
+            }
+        });
+        // Set the PhoneNumberField max width to 200.0px
+
+
         ImageIcon icon = new ImageIcon("D:\\UI-Prototype\\Prototype\\Icon\\UNFinshedBusiness.png");
         page1Frame.setIconImage(icon.getImage());
         page1Frame.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
         page1Frame.setLocationRelativeTo(null);
-
         // Initializing components
         headingLabel = new JLabel("Personal Information");
         firstNameLabel = new JLabel("First Name");
         firstName = new JTextField(20);
         middleNm = new JCheckBox("Middle name");
-        middleNm.addActionListener(e -> {
+        middleNm.addActionListener(_ -> {
             boolean isChecked = middleNm.isSelected();
             middleNameLabel.setVisible(isChecked);
             middleName.setVisible(isChecked);
         });
-
         middleNameLabel = new JLabel("Middle Name:");
         middleName = new JTextField(20);
-
         // Initially hide the middle name fields
         middleNameLabel.setVisible(false);
         middleName.setVisible(false);
-
         lastNameLabel = new JLabel("Last Name");
         lastName = new JTextField(20);
         phoneNumberLabel = new JLabel("Phone Number");
-        phoneNumber = new JTextField(20);
+
+//        // Set up the phone number field with a mask formatter
+//        try {
+//            MaskFormatter phoneFormatter = new MaskFormatter("(###) ###-####");
+////            phoneFormatter.setPlaceholderCharacter('_');
+////            phoneNumberField = new JFormattedTextField(phoneFormatter);
+//            phoneNumber.setColumns(20);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+
         emailLabel = new JLabel("Email");
         email = new JTextField(20);
-
         maleRadioButton = new JRadioButton("Male");
         femaleJRadioButton = new JRadioButton("Female");
         pntsJRadioButton = new JRadioButton("Prefer not to say");
@@ -75,22 +130,21 @@ public class FirstPage implements ActionListener, Runnable {
         radioButtonGroup.add(maleRadioButton);
         radioButtonGroup.add(femaleJRadioButton);
         radioButtonGroup.add(pntsJRadioButton);
-
         currentAddressLabel = new JLabel("Current Address:");
         numberField = new JTextField(5);
         streetField = new JTextField(15);
         cityField = new JTextField(10);
         stateField = new JTextField(2);
         zipField = new JTextField(5);
-
         weightFormattedTextField = new JFormattedTextField();
         clearButton = new JButton("Clear");
         submitButton = new JButton("Submit");
-
+        submitButton.addActionListener(_ -> {
+            submitAndNextPage();
+        });
 
         // Limit ZIP field to accept only 5 digits
         ((AbstractDocument) zipField.getDocument()).setDocumentFilter(new DocumentFilter() {
-
             public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
                 if ((fb.getDocument().getLength() + string.length()) <= 5 && string.matches("\\d*")) {
                     super.insertString(fb, offset, string, attr);
@@ -103,46 +157,46 @@ public class FirstPage implements ActionListener, Runnable {
                 }
             }
         });
+
         // Adding components with constraints
         gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 1;
-
         // Heading
         page1Frame.add(headingLabel, gbc);
-
         // First Name
         gbc.gridy = 1;
         page1Frame.add(firstNameLabel, gbc);
         gbc.gridx = 1;
         page1Frame.add(firstName, gbc);
-
         // Middle Name Checkbox (in between First and Last Name)
         gbc.gridy = 2;
         gbc.gridx = 0;
         page1Frame.add(middleNm, gbc);
-
         // Middle Name Label and Field (initially hidden)
         gbc.gridy = 3;
         gbc.gridx = 0;
         page1Frame.add(middleNameLabel, gbc);
         gbc.gridx = 1;
         page1Frame.add(middleName, gbc);
-
         // Last Name
         gbc.gridy = 4;
         gbc.gridx = 0;
         page1Frame.add(lastNameLabel, gbc);
         gbc.gridx = 1;
         page1Frame.add(lastName, gbc);
-
         // Phone Number
         gbc.gridy = 5;
         gbc.gridx = 0;
         page1Frame.add(phoneNumberLabel, gbc);
         gbc.gridx = 1;
-        page1Frame.add(phoneNumber, gbc);
+//        page1Frame.(phoneNumberField, gbc);
+//        borderPane.setBottom(phoneNumberField);
+//        page1Frame.setContentPane(borderPane);
+        gbc.gridy = 5;
+        gbc.gridx = 1;
+        page1Frame.add(phoneNumberField, gbc);
 
         // Email
         gbc.gridy = 6;
@@ -150,50 +204,41 @@ public class FirstPage implements ActionListener, Runnable {
         page1Frame.add(emailLabel, gbc);
         gbc.gridx = 1;
         page1Frame.add(email, gbc);
-
         // Sex Radio Buttons
         gbc.gridy = 7;
         gbc.gridx = 0;
         page1Frame.add(new JLabel("Sex"), gbc);
         gbc.gridx = 1;
         page1Frame.add(maleRadioButton, gbc);
-
         gbc.gridy = 8;
         page1Frame.add(femaleJRadioButton, gbc);
-
         gbc.gridy = 9;
         page1Frame.add(pntsJRadioButton, gbc);
         gbc.gridy = 12;
         gbc.gridx = 0;
         page1Frame.add(currentAddressLabel, gbc);
-
         gbc.gridy = 13;
         gbc.gridx = 0;
         page1Frame.add(new JLabel("Number"), gbc);
         gbc.gridx = 1;
         page1Frame.add(numberField, gbc);
-
         gbc.gridx = 2;
         page1Frame.add(new JLabel("Street"), gbc);
         gbc.gridx = 3;
         page1Frame.add(streetField, gbc);
-
         gbc.gridy = 14;
         gbc.gridx = 0;
         page1Frame.add(new JLabel("City"), gbc);
         gbc.gridx = 1;
         page1Frame.add(cityField, gbc);
-
         gbc.gridx = 2;
         page1Frame.add(new JLabel("State"), gbc);
         gbc.gridx = 3;
         page1Frame.add(stateField, gbc);
-
         gbc.gridx = 4;
         page1Frame.add(new JLabel("ZIP"), gbc);
         gbc.gridx = 5;
         page1Frame.add(zipField, gbc);
-
         // Buttons (Clear and Submit)
         gbc.gridy = 11;
         gbc.gridx = 0;
@@ -203,21 +248,23 @@ public class FirstPage implements ActionListener, Runnable {
         clearButton.addActionListener(_ -> {
             clearForm();
         });
-
         // Final frame setup
         page1Frame.pack();
         page1Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         page1Frame.setVisible(true);
     }
 
-    @Override
-    public void run() {
-    }
+    private void submitAndNextPage() {
+        String mName = null;
+        String fName = firstName.getText();
+        if (middleNm.isSelected()) {
+            mName = middleName.getText();
+        }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+        String lName = lastName.getText();
+        String phoneNmber = phoneNumberField.getText();
+        System.out.println(fName + " " + (mName != null ? mName + " " : "") + lName + phoneNmber);
     }
-
 
     private void clearForm() {
         // Iterate through each component in page1Frame
@@ -232,11 +279,28 @@ public class FirstPage implements ActionListener, Runnable {
                 ((JCheckBox) component).setSelected(false); // Deselect checkboxes
             }
         }
-
         // Clear button groups separately
         if (radioButtonGroup != null) {
             radioButtonGroup.clearSelection(); // Clear radio button selection
         }
     }
 
+    private void formatPhoneNumber() {
+        String rawNumber = phoneNumberField.getText();
+        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+
+        try {
+            // Parse number (assuming default country as 'US' for this example)
+            Phonenumber.PhoneNumber numberProto = phoneUtil.parse(rawNumber, "US");
+
+            if (phoneUtil.isValidNumber(numberProto)) {
+                String formattedNumber = phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+                phoneNumberField.setValue(formattedNumber);  // Sets formatted value
+            } else {
+                JOptionPane.showMessageDialog(page1Frame, "Invalid phone number!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (NumberParseException e) {
+            JOptionPane.showMessageDialog(page1Frame, "Error parsing phone number!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
