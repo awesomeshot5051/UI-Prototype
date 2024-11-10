1bfeca17b242fd80bedca78517bfca33d4d12f1f
s412924 <traeengland01@Att.net>
2024-11-09 19:41:36 -0500

    Added getStates method and getIcon methods so it's easier to retrieve all the states and the icon image respectively.

--- Prototype/src/main/java/UIPrototypeMainClass.java
+++ Prototype/src/main/java/UIPrototypeMainClass.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class UIPrototypeMainClass {
+    static ImageIcon icon = new ImageIcon("D:\\UI-Prototype\\Prototype\\Icon\\UNFinshedBusiness.png");
+
     public static void main(String[] args) {
         new FirstPage();
+    }
+
+    static JComboBox<String> getStates() {
+        String[] states = {"Select State", "Alabama", "Alaska", "Arizona", "Arkansas", "California",
+                "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+                "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+                "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+                "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+                "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+                "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
+                "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+                "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+                "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
+        return new JComboBox<>(states);
+    }
+
+    public static Image getIcon() {
+        return icon.getImage();
     }
 }
